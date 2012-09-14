84092e5dae0f7b27bfddaf446e66c0ca648fef2f
Arthur Neves <arthur@500px.com>
2012-09-14 14:30:16 -0400

    more local

--- lib/capistrano-resque/capistrano_integration.rb
+++ lib/capistrano-resque/capistrano_integration.rb
@@ -10,19 +10,8 @@ module CapistranoResque
         _cset(:app_env) { fetch(:rails_env, "production") }
         _cset(:verbosity, 1)
 
-        # def remote_file_exists?(full_path)
-        #   "true" ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
-        # end
-
-        def remote_process_exists?(pid_file)
-          capture("ps -p $(cat #{pid_file}) ; true").strip.split("\n").size == 2
-        end
-
-        def current_pids
-          capture("ls #{current_path}/tmp/pids/resque_work*.pid 2>/dev/null || true").strip.split(/\r{0,1}\n/)
-        end
-        
         def workers_roles
+          workers = fetch(:workers)
           return workers.keys if workers.first[1].is_a? Hash
           [:resque_worker]
         end
@@ -40,21 +29,15 @@ module CapistranoResque
 
         namespace :resque do
           desc "See current worker status"
-          task :status, :roles => workers_roles do
-            current_pids.each do |pid|
-              if remote_file_exists?(pid)
-                if remote_process_exists?(pid)
-                  logger.important("Up and running", "Resque Worker: #{pid}")
-                else
-                  logger.important("Down", "Resque Worker: #{pid}")
-                end
-              end
-            end
+          task :status, :roles => lambda { workers_roles() }, :on_no_matching_servers => :continue do
+            command = "if [ -e /www/500px/tmp/pids/resque_work_1.pid ]; then \
+                for f in $(ls #{current_path}/tmp/pids/resque_work*.pid); do ps -p $(cat $f) | sed -n 2p ;done \
+               ;fi"
+            run(command)
           end
 
           desc "Start Resque workers"
-          task :start, :roles => workers_roles do
-            
+          task :start, :roles => lambda { workers_roles() }, :on_no_matching_servers => :continue do
             for_each_workers do |role, workers|
               worker_id = 1
               workers.each_pair do |queue, number_of_workers|
@@ -62,39 +45,24 @@ module CapistranoResque
                 number_of_workers.times do
                   pid = "./tmp/pids/resque_work_#{worker_id}.pid"
                   run("cd #{current_path} && RAILS_ENV=#{app_env} QUEUE=\"#{queue}\" \
-                   PIDFILE=#{pid} BACKGROUND=yes VERBOSE=1 bundle exec rake environment resque:work >> #{shared_path}/log/resque.log 2>&1 &", 
+                   PIDFILE=#{pid} BACKGROUND=yes VERBOSE=1 #{fetch(:bundle_cmd, "bundle")} exec rake environment resque:work >> #{shared_path}/log/resque.log 2>&1 &", 
                       :roles => role)
                   worker_id += 1
                 end
               end
             end
-
           end
 
           desc "Quit running Resque workers"
-          task :stop, :roles => workers_roles do
-            
-            run "for f in `ls #{current_path}/tmp/pids/resque_work*.pid`; do kill `cat $f` ; rm $f ;done "
-            
-            # current_pids.each do |pid_file|
-            #           pid = pid_file.strip.split(/\r{0,1}\n/)
-            #           # if remote_file_exists?(pid)
-            #             # if remote_process_exists?(pid)
-            #               logger.important("Stopping...", "Resque Worker: #{pid}")
-            #               run "#{try_sudo} kill `cat #{pid}`"
-            #           run "if [ -f #{pid_file} ]; then #{try_sudo} kill `cat #{pid_file}` ; fi"
-            #             # else
-            #             #   run "rm #{pid}"
-            #             #   logger.important("Resque Worker #{pid} is not running.", "Resque")
-            #             # end
-            #           # else
-            #           #   logger.important("No PIDs found. Check if Resque is running.", "Resque")
-            #           # end
-            #         end
+          task :stop, :roles => lambda { workers_roles() }, :on_no_matching_servers => :continue do
+            command = "if [ -e /www/500px/tmp/pids/resque_work_1.pid ]; then \
+              for f in `ls #{current_path}/tmp/pids/resque_work*.pid`; do #{try_sudo} kill `cat $f` ; rm $f ;done \
+              ;fi"
+            run(command)
           end
 
           desc "Restart running Resque workers"
-          task :restart, :roles => workers_roles do
+          task :restart, :roles => lambda { workers_roles() }, :on_no_matching_servers => :continue do
             stop
             start
           end
@@ -103,7 +71,7 @@ module CapistranoResque
             desc "Starts resque scheduler with default configs"
             task :start, :roles => :resque_scheduler do
               run "cd #{current_path} && RAILS_ENV=#{app_env} \
-PIDFILE=./tmp/pids/scheduler.pid BACKGROUND=yes bundle exec rake resque:scheduler >> #{shared_path}/log/resque_scheduler.log 2>&1 &"
+PIDFILE=./tmp/pids/scheduler.pid BACKGROUND=yes #{fetch(:bundle_cmd, "bundle")} exec rake resque:scheduler >> #{shared_path}/log/resque_scheduler.log 2>&1 &"
             end
 
             desc "Stops resque scheduler"
